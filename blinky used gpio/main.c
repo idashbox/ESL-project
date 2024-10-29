2f06ab946ce7be047da2694bb0df18004e2d5c75
idashbox <dmerkusina@gmail.com>
2024-10-29 16:32:01 +0300

    Update main blinky gpio

--- blinky used gpio/main.c
+++ blinky used gpio/main.c
@@ -9,87 +9,136 @@
 #define LED_2_G_PIN NRF_GPIO_PIN_MAP(1, 9)
 #define LED_2_B_PIN NRF_GPIO_PIN_MAP(0, 12)
 
-void led_on(uint32_t pin_number)
+void hal_gpio_init_output(uint32_t pin_number)
 {
-    nrf_gpio_pin_clear(pin_number);
+    nrf_gpio_cfg_output(pin_number);
 }
 
-void led_off(uint32_t pin_number)
+void hal_gpio_init_input(uint32_t pin_number, uint32_t pull_config)
+{
+    nrf_gpio_cfg_input(pin_number, pull_config);
+}
+
+void hal_gpio_set(uint32_t pin_number)
 {
     nrf_gpio_pin_set(pin_number);
 }
 
-void led_toggle(uint32_t pin_number)
+void hal_gpio_clear(uint32_t pin_number)
+{
+    nrf_gpio_pin_clear(pin_number);
+}
+
+void hal_gpio_toggle(uint32_t pin_number)
 {
     nrf_gpio_pin_toggle(pin_number);
 }
 
+bool hal_gpio_read(uint32_t pin_number)
+{
+    return nrf_gpio_pin_read(pin_number);
+}
+
+void led_on(uint32_t pin_number)
+{
+    hal_gpio_clear(pin_number);
+}
+
+void led_off(uint32_t pin_number)
+{
+    hal_gpio_set(pin_number);
+}
+
+void led_toggle(uint32_t pin_number)
+{
+    hal_gpio_toggle(pin_number);
+}
+
 bool is_button_pressed()
 {
-    return !nrf_gpio_pin_read(BUTTON_PIN);
+    return !hal_gpio_read(BUTTON_PIN);
+}
+
+void initialize_gpio()
+{
+    hal_gpio_init_output(LED_1_PIN);
+    hal_gpio_init_output(LED_2_R_PIN);
+    hal_gpio_init_output(LED_2_G_PIN);
+    hal_gpio_init_output(LED_2_B_PIN);
+    hal_gpio_init_input(BUTTON_PIN, NRF_GPIO_PIN_PULLUP);
+}
+
+void handle_button_press(uint8_t *current_led, bool *led_on_flag, uint8_t *blinks, size_t blinks_size)
+{
+    if (*current_led == 0)
+    {
+        led_toggle(LED_1_PIN);
+    }
+    else if (*current_led == 1)
+    {
+        led_toggle(LED_2_R_PIN);
+    }
+    else if (*current_led == 2)
+    {
+        led_toggle(LED_2_G_PIN);
+    }
+    else
+    {
+        led_toggle(LED_2_B_PIN);
+    }
+    *led_on_flag = !*led_on_flag;
+    nrf_delay_ms(500);
+
+    if (*led_on_flag)
+    {
+        blinks[*current_led]--;
+        if (blinks[*current_led] == 0)
+        {
+            *current_led = (*current_led + 1) % blinks_size;
+            blinks[*current_led] = (*current_led == 0) ? 6 : (*current_led == 1) ? 5 : (*current_led == 2) ? 8 : 1;
+
+            if (*current_led == 0)
+            {
+                led_off(LED_1_PIN);
+            }
+            else if (*current_led == 1)
+            {
+                led_off(LED_2_R_PIN);
+            }
+            else if (*current_led == 2)
+            {
+                led_off(LED_2_G_PIN);
+            }
+            else
+            {
+                led_off(LED_2_B_PIN);
+            }
+        }
+    }
+}
+
+void turn_off_all_leds()
+{
+    led_off(LED_1_PIN);
+    led_off(LED_2_R_PIN);
+    led_off(LED_2_G_PIN);
+    led_off(LED_2_B_PIN);
 }
 
 int main(void)
 {
-    nrf_gpio_cfg_output(LED_1_PIN);
-    nrf_gpio_cfg_output(LED_2_R_PIN);
-    nrf_gpio_cfg_output(LED_2_G_PIN);
-    nrf_gpio_cfg_output(LED_2_B_PIN);
-    nrf_gpio_cfg_input(BUTTON_PIN, NRF_GPIO_PIN_PULLUP);
+    initialize_gpio();
 
     uint8_t blinks[] = {6, 5, 8, 1};
     uint8_t current_led = 0;
     bool led_on_flag = false;
+    size_t blinks_size = sizeof(blinks) / sizeof(blinks[0]);
 
     while (true)
     {
         if (is_button_pressed())
         {
-            if (current_led == 0)
-            {
-                led_toggle(LED_1_PIN);
-            }
-            else if (current_led == 1)
-            {
-                led_toggle(LED_2_R_PIN);
-            }
-            else if (current_led == 2)
-            {
-                led_toggle(LED_2_G_PIN);
-            }
-            else
-            {
-                led_toggle(LED_2_B_PIN);
-            }
-            led_on_flag = !led_on_flag;
-            nrf_delay_ms(500);
-
-            if (led_on_flag)
-            {
-                blinks[current_led]--;
-                if (blinks[current_led] == 0)
-                {
-                    current_led = (current_led + 1) % (sizeof(blinks) / sizeof(blinks[0]));
-                    blinks[current_led] = (current_led == 0) ? 6 : (current_led == 1) ? 5 : (current_led == 2) ? 8 : 1;
-
-                    if (current_led == 0)
-                    {
-                        led_off(LED_1_PIN);
-                    }
-                    else if (current_led == 1)
-                    {
-                        led_off(LED_2_R_PIN);
-                    }
-                    else if (current_led == 2)
-                    {
-                        led_off(LED_2_G_PIN);
-                    }
-                    else
-                    {
-                        led_off(LED_2_B_PIN);
-                    }
-                }
-            }
+            handle_button_press(&current_led, &led_on_flag, blinks, blinks_size);
         }
         else if (led_on_flag)
         {
@@ -97,10 +146,7 @@ int main(void)
         }
         else
         {
-            led_off(LED_1_PIN);
-            led_off(LED_2_R_PIN);
-            led_off(LED_2_G_PIN);
-            led_off(LED_2_B_PIN);
+            turn_off_all_leds();
         }
     }
 }
