738994955cff51d19c0b3f1a0d476baf195bceeb
idashbox <dmerkusina@gmail.com>
2025-03-05 08:12:27 +0000

    Task 10. Create BLE Service based on estc_service example

--- estc_service/estc_service.c
+++ estc_service/estc_service.c
@@ -1,23 +1,57 @@
 #include "estc_service.h"
-
-#include "app_error.h"
+#include <string.h>
 #include "nrf_log.h"
-
-#include "ble.h"
-#include "ble_gatts.h"
 #include "ble_srv_common.h"
+#include "nrf_ble_gatt.h"
+#include "app_error.h"
 
-ret_code_t estc_ble_service_init(ble_estc_service_t *service)
+static uint32_t char_value = 0;
+
+uint32_t estc_ble_service_init(ble_estc_service_t *p_service)
 {
-    ret_code_t error_code = NRF_SUCCESS;
+    if (p_service == NULL)
+    {
+        return NRF_ERROR_NULL;
+    }
 
+    ret_code_t err_code;
     ble_uuid_t service_uuid;
-    // TODO: 3. Add service UUIDs to the BLE stack table using `sd_ble_uuid_vs_add`
-    // TODO: 4. Add service to the BLE stack using `sd_ble_gatts_service_add`
+    ble_uuid128_t base_uuid = {CUSTOM_SERVICE_UUID_BASE};
 
-    // NRF_LOG_DEBUG("%s:%d | Service UUID: 0x%04x", __FUNCTION__, __LINE__, service_uuid.uuid);
-    // NRF_LOG_DEBUG("%s:%d | Service UUID type: 0x%02x", __FUNCTION__, __LINE__, service_uuid.type);
-    // NRF_LOG_DEBUG("%s:%d | Service handle: 0x%04x", __FUNCTION__, __LINE__, service->service_handle);
+    err_code = sd_ble_uuid_vs_add(&base_uuid, &p_service->uuid_type);
+    APP_ERROR_CHECK(err_code);
 
+    service_uuid.type = p_service->uuid_type;
+    service_uuid.uuid = CUSTOM_SERVICE_UUID;
+
+    err_code = sd_ble_gatts_service_add(BLE_GATTS_SRVC_TYPE_PRIMARY, &service_uuid, &p_service->service_handle);
+    APP_ERROR_CHECK(err_code);
+
+    ble_gatts_char_md_t char_md = {0};
+    ble_gatts_attr_md_t attr_md = {0};
+    ble_gatts_attr_t attr_char_value = {0};
+    ble_uuid_t char_uuid;
+
+    char_uuid.type = p_service->uuid_type;
+    char_uuid.uuid = CUSTOM_CHAR_UUID;
+
+    char_md.char_props.read = 1;
+    char_md.char_props.write = 1;
+
+    attr_md.read_perm = (ble_gap_conn_sec_mode_t){1, 1};
+    attr_md.write_perm = (ble_gap_conn_sec_mode_t){1, 1};
+    attr_md.vloc = BLE_GATTS_VLOC_STACK;
+
+    attr_char_value.p_uuid = &char_uuid;
+    attr_char_value.p_attr_md = &attr_md;
+    attr_char_value.init_len = sizeof(uint32_t);
+    attr_char_value.init_offs = 0;
+    attr_char_value.max_len = sizeof(uint32_t);
+    attr_char_value.p_value = (uint8_t *) &char_value;
+
+    err_code = sd_ble_gatts_characteristic_add(p_service->service_handle, &char_md, &attr_char_value, &p_service->char_handles);
+    APP_ERROR_CHECK(err_code);
+
+    NRF_LOG_INFO("Custom BLE service initialized.");
     return NRF_SUCCESS;
 }
--- estc_service/estc_service.h
+++ estc_service/estc_service.h
@@ -2,19 +2,19 @@
 #define ESTC_SERVICE_H__
 
 #include <stdint.h>
-
 #include "ble.h"
 #include "sdk_errors.h"
+#include "ble_gatts.h"
 
-// TODO: 1. Generate random BLE UUID (Version 4 UUID) and define it in the following format:
-// #define ESTC_BASE_UUID { 0xF6, 0xCE, 0x0F, 0xC4, 0xCE, 0x9F, /* - */ 0xC3, 0x99, /* - */ 0xF7, 0x4D, /* - */ 0xDB, 0xB9, /* - */ 0x00, 0x00, 0xEC, 0x39 } // UUID: EC39xxxx-B9DB-4DF7-99C3-9FCEC40FCEF6
-
-// TODO: 2. Pick a random service 16-bit UUID and define it:
-// #define ESTC_SERVICE_UUID 0xabcd
+#define CUSTOM_SERVICE_UUID_BASE { 0xE8, 0x66, 0x78, 0x47, 0x5A, 0xDB, 0x4A, 0xAC, 0xB5, 0x6F, 0xB8, 0x81, 0x37, 0x51, 0x29, 0x3B }
+#define CUSTOM_SERVICE_UUID 0x1400
+#define CUSTOM_CHAR_UUID 0x1401 
 
 typedef struct
 {
     uint16_t service_handle;
+    ble_gatts_char_handles_t char_handles;
+    uint8_t uuid_type;
 } ble_estc_service_t;
 
 ret_code_t estc_ble_service_init(ble_estc_service_t *service);
